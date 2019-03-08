73148b4422d25f9340a9135d183a60102c9782c5
Altynbek <altynbek.topayev@gmail.com>
2019-03-08 23:28:22 +0600

    Trying to add "multiplication" function prototype

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 import Matrix
 
+matrixA = Matrix.Matrix(2,2)
+matrixB = Matrix.Matrix(2,2)
+
+# Entering data
 for i in range(2):
     print('Type values for Matrix %d: ' % (i + 1))
     a = input("Enter the number of rows: ")
@@ -7,8 +11,14 @@ for i in range(2):
 
     try:
         A = Matrix.Matrix(int(a), int(b))
-        # print(A.display())
+        print(A.display())
         A.filling()
+        if i == 0:
+            matrixA = A
+        else:
+            matrixB = A
         print(A.display())
     except ValueError:
         print("Please enter a number!")
+
+Matrix.multiplication(matrixA, matrixB))
--- Matrix.py
+++ Matrix.py
@@ -2,18 +2,24 @@ class Matrix:
     # Parent (square matrix)
 
     def __init__(self, row, column):
-        self.dimension = (row, column)          # Dimension of created square matrix
+        self.dimension = (row, column)                          # Dimension of created square matrix
         self.data = [[0]*column for i in range(row)]            # Default filling with 0 values
 
-    def display(self) -> object:            # Printing a matrix
-        for i in range(len(self.data)):
+    # Printing a matrix
+    def display(self) -> object:
+        for i in range(len(self.data)):  # ! Make here using enumerate
             print(self.data[i])
 
-    def filling(self):          # Filling elements i-row j-column
-        for i in range(len(self.data)):
-            for j in range(len(self.data[i])):
+    # Filling elements i-rows j-columns
+    def filling(self):
+        for i in range(len(self.data)):  # ! Make here using enumerate
+            for j in range(len(self.data[i])):  # ! Make here using enumerate
                 print('Type value for element X%d%d: ' % (i+1, j+1))
                 x = input()
                 self.data[i][j] = int(x)
 
-    def multiplication(self, matrixA, matrixB):         # The multiplication of two matrices
+    # The multiplication of two matrices
+    def multiplication(self, matrix_a, matrix_b):
+        print('matrix A: ' + matrix_a)
+        print('MULTIPLICATION')
+        print('matrix B: ' + matrix_b)
