333d1256a94a947cfa2b190f7861cf0e12447eb6
Altynbek_Topayev <altynbek_topayev@gmail.com>
2019-03-13 18:48:06 +0600

    Starting to implement the main function (multiplication)

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import Matrix
 
+matrix_a = Matrix.Matrix(2, 2)
+matrix_b = Matrix.Matrix(0, 0)
+
 for i in range(2):
     print('Type values for Matrix %d: ' % (i + 1))
     a = input("Enter the number of rows: ")
@@ -7,8 +10,14 @@ for i in range(2):
 
     try:
         A = Matrix.Matrix(int(a), int(b))
-        # print(A.display())
-        A.filling()
-        print(A.display())
+        A.fill()
+        A.display()
+        if i == 0:
+            matrix_a = A
+        else:
+            matrix_b = A
     except ValueError:
-        print("Please enter a number!")
+        print("Please enter an integer value!")
+        break
+
+Matrix.Matrix.multiplication(matrix_a, matrix_b)
--- Matrix.py
+++ Matrix.py
@@ -1,19 +1,38 @@
 class Matrix:
-    # Parent (square matrix)
+    # Class for operations under matrices
 
     def __init__(self, row, column):
-        self.dimension = (row, column)          # Dimension of created square matrix
-        self.data = [[0]*column for i in range(row)]            # Default filling with 0 values
+        self.dimension = (row, column)      # Dimension of created square matrix
+        self.data = [[0]*column for i in range(row)]        # Matrix list, default filling with 0 values
 
-    def display(self) -> object:            # Printing a matrix
+    # Printing a matrix
+    def display(self) -> object:
         for i in range(len(self.data)):
             print(self.data[i])
 
-    def filling(self):          # Filling elements i-row j-column
+    # Filling elements i-row j-column
+    def fill(self):
         for i in range(len(self.data)):
             for j in range(len(self.data[i])):
                 print('Type value for element X%d%d: ' % (i+1, j+1))
                 x = input()
                 self.data[i][j] = int(x)
 
-    def multiplication(self, matrixA, matrixB):         # The multiplication of two matrices
+    # The multiplication of two matrices
+    @staticmethod
+    def multiplication(a, b):
+        rows_a = len(int(a.dimension))
+        columns_a = len(int(a.dimension[1]))
+        rows_b = len(int(b.dimension))
+        columns_b = len(int(b.dimension[1]))
+
+        c = [[0 for row in range(columns_b)] for col in range(rows_a)]
+
+        if rows_a == columns_b and columns_a == rows_b:
+            for i in range(rows_a):
+                for j in range(columns_b):
+                    for k in range(columns_a):
+                        c[i][j] += a[i][k] * b[k][j]
+            c.display()
+        else:
+            print("Matrices are not compatible to multiply!")
